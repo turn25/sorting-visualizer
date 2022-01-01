883a159f35465b0dd4f6a6c7e47692430a48eeb8
vuquangtuan123 <vuquangtuan12345@gmail.com>
2022-01-01 08:39:27 +0700

    fix: footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full flex justify-center items-center text-gray-600 bg-stone-50 p-2 rounded-t-sm shadow-md cursor-default">
-      {new Date().getFullYear()}&copy;{" "}
+      {new Date().getFullYear()} &copy;&nbsp;
       <a
         href="https://github.com/vuquangtuan123"
         rel="noreferrer"
