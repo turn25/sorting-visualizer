4ef21a72b6e74824b367892560e9b6617ce3699d
vuquangtuan123 <vuquangtuan12345@gmail.com>
2022-01-04 19:50:46 +0700

    fix: footer copyright symbol position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full flex justify-center items-center text-gray-600 bg-stone-50 p-2 rounded-t-sm shadow-md cursor-default">
-      {new Date().getFullYear()} &copy;&nbsp;
+      &copy; {new Date().getFullYear()}&nbsp;
       <a
         href="https://github.com/vuquangtuan123"
         rel="noreferrer"
